ecf577a774f8718184aed604c6c3009c72d456a9
Giovane Oba <82154690+GiovaneOba@users.noreply.github.com>
2025-05-05 21:04:46 -0300

    Logica de Acertou e Errou.asm

--- jogo_adivinha.asm
+++ jogo_adivinha.asm
@@ -33,7 +33,7 @@ EscreveLinha2: CLR     A
        
 AguardaComecar:
         CALL    ScanKeyPad      
-        CJNE    R7,#'1',AguardaComecar 
+        CJNE    R7,#1,AguardaComecar 
         CLR     RS
 		SETB    0h              
         MOV     A,#01h          
@@ -43,18 +43,87 @@ AguardaComecar:
 Proximo: CALL    ScanKeyPad      
          SetB    RS              
          CLR     A
-         MOV     A,R7            
-         CALL    SendChar        
-         CJNE    R7,#'#',Proximo    
+         CALL    teste12
+		                    
+    ;     CJNE    R7,#'#',Proximo    
               
        
               
 EndHere:
-        JMP     $               
+        JMP     $   
 ;------------------------------ *Fim da Main* -----------------------------------
 
 
 ;--------------------------------- Sub-rotinas ----------------------------------
+teste12:
+	MOV A, R6     ; A = valor1
+	SUBB A, R7    ; A = A - R7 (com carry)
+	JNZ erro      ; Se o resultado não é zero -> são diferentes
+	; Se cair aqui, é porque são iguais
+	LCALL correto
+	
+
+erro:
+       SetB RS              
+        MOV     DPTR,#LINHA3      
+EscreveLinha12: CLR     A               
+        MOVC    A,@A+DPTR       
+        JZ      ProximaLinha2        
+        CALL    SendChar        
+        INC     DPTR            
+        JMP     EscreveLinha12          
+       
+ProximaLinha2:
+        CALL    CursorPos       
+        SetB    RS              
+        MOV     DPTR,#LINHA5      
+EscreveLinha22: CLR     A
+        MOVC    A,@A+DPTR       
+        JZ      Proximo       
+        CALL    SendChar        
+        INC     DPTR
+      	JMP     EscreveLinha22
+
+correto:
+SetB RS              
+        MOV     DPTR,#LINHA3      
+EscreveLinha13: CLR     A               
+        MOVC    A,@A+DPTR       
+        JZ      ProximaLinha3        
+        CALL    SendChar        
+        INC     DPTR            
+        JMP     EscreveLinha13          
+       
+ProximaLinha3:
+        CALL    CursorPos       
+        SetB    RS              
+        MOV     DPTR,#LINHA4      
+EscreveLinha23: CLR     A
+        MOVC    A,@A+DPTR       
+        JZ      Main       
+        CALL    SendChar        
+        INC     DPTR
+      	JMP     EscreveLinha23
+
+        
+
+escreveErro:
+        CLR A
+        MOVC A, @A + DPTR
+		JZ      ProximaLinha        
+        CALL    SendChar        
+        INC     DPTR            
+        JMP     escreveErro
+       ; Carrega o primeiro caractere da string em A
+;EscreveTexto:
+ ;       JZ FimTexto               ; Se A == 0 (fim da string), sai do loop
+  ;      CALL SendChar             ; Envia o caractere para o LCD
+   ;     INC DPTR                  ; Avança o ponteiro para o próximo caractere
+    ;    MOVC A, @A + DPTR         ; Carrega o próximo caractere de DPTR
+     ;   JMP EscreveTexto          ; Continua o loop
+;FimTexto:
+;        RET
+
 FuncSet:
         CLR     P1.7            
         CLR     P1.6
@@ -156,14 +225,14 @@ Delay:
 ScanKeyPad:
 		JB      0,VerificaTecla
         MOV     A, R6
-        CJNE    A, #100, Continua
+        CJNE    A, #9, Continua
         MOV     R6, #0
         JMP     Continua
 
 Continua:
         INC     R6
         CLR     F0        
-        CJNE    R7, #'1', VerificaTecla
+        CJNE    R7, #1, VerificaTecla
         ; Quando R7 for igual a '1', executa normalmente
 
 VerificaTecla:
@@ -192,9 +261,9 @@ VerificaTecla:
 
 Feito:   
         CLR     F0        
-        CJNE    R7, #'1', Retorna
+        CJNE    R7, #1, Retorna
         MOV     A, R6           
-        CJNE    A, #100, Retorna
+        CJNE    A, #9, Retorna
         MOV     R6, #0       
 
 Retorna:
@@ -226,41 +295,41 @@ IDCode3:
         RET
 
 Tecla03:        SETB    F0      
-                MOV     R7,#'3'
+                MOV     R7,#3
 JNB P0.4,$ 
                 RET
 Tecla13:        SETB    F0
-                MOV     R7,#'2'
+                MOV     R7,#2
 JNB P0.5,$ 
                 RET
 Tecla23:        SETB    F0
-                MOV     R7,#'1'
+                MOV     R7,#1
 JNB P0.6,$ 
                 RET
 
 Tecla02:        SETB    F0
-                MOV     R7,#'6' 
+                MOV     R7,#6 
 JNB P0.4,$
                 RET
 Tecla12:        SETB    F0
-                MOV     R7,#'5' 
+                MOV     R7,#5 
 JNB P0.5,$
                 RET
 Tecla22:        SETB    F0
-                MOV     R7,#'4' 
+                MOV     R7,#4 
 JNB P0.6,$
                 RET
 
 Tecla01:        SETB    F0
-                MOV     R7,#'9' 
+                MOV     R7,#9
 JNB P0.4,$
                 RET
 Tecla11:        SETB    F0
-                MOV     R7,#'8' 
+                MOV     R7,#8 
 JNB P0.5,$
                 RET
 Tecla21:        SETB    F0
-                MOV     R7,#'7'
+                MOV     R7,#7
 JNB P0.6,$
                 RET
 
@@ -269,7 +338,7 @@ Tecla00:        SETB    F0
 JNB P0.4,$
                 RET
 Tecla10:        SETB    F0
-                MOV     R7,#'0'
+                MOV     R7,#0
 JNB P0.5,$  
                 RET
 Tecla20:        SETB    F0
@@ -284,5 +353,8 @@ JNB P0.6,$
                               ORG     0200h          
 LINHA1:       DB 'B','E','M',' ','V','I','N','D','O',0
 LINHA2:       DB 'J','O','G','O',' ','A','D','I','V','I','N','H','A',0
+LINHA4: DB 'A','C','E','R','T','O','U',0
+LINHA5: DB 'I','N','C','O','R','R','E','T','O',0
+LINHA3: DB 'V','O','C','E',0
        
 END
